f69bf05237f9eff99854484d38f86191af7bf4c7
Leo <leonardo.r.s.duarte@gmail.com>
2023-12-05 12:05:50 -0300

    Primeiro projeto Ola Mundo em Java

---
+++ Java React/Projetos Java/Familia71/src/aulasJava/olaMundo.java
@@ -0,0 +1,31 @@
+package aulasJava;
+
+import java.util.Scanner;
+
+public class olaMundo {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String nome = "Carol Ferreira";
+		int idade = 18;
+		float altura = 1.70f;
+		final float dinheiro = 0f; 	// final = constante 
+		
+		// ---------------------------------------------
+	
+		System.out.println("Digite seu nome: ");
+		nome = entrada.nextLine();
+		
+		System.out.println("Digite sua idade: ");
+		idade = entrada.nextInt();
+		
+		System.out.println("Meu nome é: " + nome);
+		System.out.println("Tenho " + idade + " anos");
+		
+		
+		entrada.close();
+	}
+
+}
