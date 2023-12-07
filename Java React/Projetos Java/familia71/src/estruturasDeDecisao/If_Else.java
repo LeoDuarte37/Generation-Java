e14fc898b1fbc902d85f8b1c98faf30cbfc2adbe
Leo <leonardo.r.s.duarte@gmail.com>
2023-12-07 11:26:39 -0300

    Estruturas de decisão

---
+++ Java React/Projetos Java/familia71/src/estruturasDeDecisao/If_Else.java
@@ -0,0 +1,35 @@
+package estruturasDeDecisao;
+
+import java.util.Scanner;
+
+public class If_Else {
+
+	public static void main(String[] args) {
+		
+		float n1, n2, n3, media;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Digite a primeira nota: ");
+		n1 = ler.nextFloat();
+		
+		System.out.println("Digite a segunda nota: ");
+		n2 = ler.nextFloat();
+		
+		System.out.println("Digite a terceira nota: ");
+		n3 = ler.nextFloat();
+		
+		media = (n1 + n2 + n3) / 3;;
+		
+		if (media >= 7 && media <= 10) {
+			System.out.println("\nAprovado!");
+			
+		} else if (media >= 5) {
+			System.out.println("\nRecuperação...");
+			
+		} else {
+			System.out.println("\nReprovado :(");
+		}
+	}
+
+}
---
+++ Java React/Projetos Java/familia71/src/estruturasDeDecisao/SwitchCase.java
@@ -0,0 +1,40 @@
+package estruturasDeDecisao;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		
+		int op;
+		
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("\n\tMenu de opções");
+		System.out.println("\n1 - Cadastrar");
+		System.out.println("\n2 - Transferência");
+		System.out.println("\n3 - Ver saldo");
+		
+		System.out.println("\nDigite uma opção:");
+		op = ler.nextInt();
+		
+		switch(op) {
+		case 1:
+			System.out.println("Cadastro realizado!");
+			break;
+			
+		case 2: 
+			System.out.println("Transferência realizada!");
+			break;
+			
+		case 3: 
+			System.out.println("Seu saldo é: ");
+			break;
+			
+		default:
+			System.out.println("Opção inválida");
+			
+		}
+	}
+
+}
